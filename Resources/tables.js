d4730f7fa8fc367b701d2f18608a2615b97f930a
Jarrod Smith <jasmith2@fullsail.edu>
2015-12-05 17:11:43 -0600

    Finale code
    Complete code in working order. With full comments.

--- Resources/tables.js
+++ Resources/tables.js
@@ -24,7 +24,6 @@ var wearable =[{name: "Samsung Gear S2", Screen: "1.2 inch Super Amoled display"
 		
 
 //Create Table view for phones
-
 var devices = Ti.UI.createTableView({
 	top: titleViewBorder.top + titleViewBorder.height
 });
@@ -34,6 +33,7 @@ if(Ti.Platform === "iPhone OS"){
 	devices.style = Ti.UI.iPhone.TableViewStyle.GROUPED;
 };
 
+	//Function for my event listener
 	var specs = function(){ 
 		var specWindow = Ti.UI.createWindow({
 			backgroundColor: "#333"
@@ -53,44 +53,148 @@ if(Ti.Platform === "iPhone OS"){
 			style: "bold",
 			top: 40
 		});
-		
+			
 		//Border for bottom of the title bar
 		var specTitleViewBorder = Ti.UI.createView({
-			backgroundColor: "#002699",
+			backgroundColor: "#fff",
 			height: 1,
 			top: titleBarView.height + titleBarView.top
 		});
 		
+		//Creates view for back button
 		var backButton = Ti.UI.createView({
 			backgroundColor: "#d9e6f2",
 			width: 100,
-			height: 50,
-			bottom: 15,
-			left: 15
+			height: 40,
+			bottom: 10,
+			left: 10
 		});
 		
-		var backButtonLabel = Ti.UI.createLabel({
-			text: "Back",
-			fontSize: 14,
-			fontFamily: "Ariel"
+			//Creates label for back button
+			var backButtonLabel = Ti.UI.createLabel({
+				text: "Back",
+				fontSize: 14,
+				fontFamily: "Ariel"
+			});
+			
+		//Creates label for screen size
+		var screenSizeLabel = Ti.UI.createLabel({
+			text: "Screen Size:",
+			left:20
 		});
+			
+			//creates view for screen size label
+			var screenSizeLabelView = Ti.UI.createView({
+				backgroundColor: "#d9e6f2",
+				height:30,
+				top: specTitleViewBorder.top + 20,
+			});	
 				
-				
-				var specifics = Ti.UI.createLabel({
-					 text: this.internMem,
-					 color: "white",
-					 top: specTitleViewBorder + 20
+				//pulls screen size info from array	
+				var screenSizeSpec = Ti.UI.createLabel({
+					text: this.screenSize,
+					color: "white",
+					top: screenSizeLabelView.top + screenSizeLabelView.height + 10,
+					left:20
 				});
+			
+		//Creates label for ram size	
+		var internalMemLabel = Ti.UI.createLabel({
+			text: "Internal Memory:",
+			left:20
+		});
+			
+			//creates view for ram label
+			var internalMemLabelView = Ti.UI.createView({
+				backgroundColor: "#d9e6f2",
+				height:30,
+				top: screenSizeSpec.top + 40,
+			});	
+			
+				//pulls ram info from array	
+				var internalMemSpec = Ti.UI.createLabel({
+					text: this.internalMem,
+					color: "white",
+					top: internalMemLabelView.top + internalMemLabelView.height + 10,
+					left:20
+				});
+		
+		//Creates label for camera size		
+		var ramSpecLabel = Ti.UI.createLabel({
+			text: "Ram:",
+			left:20
+		});
+		
+			//creates view for camera label
+			var ramSpecLabelView = Ti.UI.createView({
+				backgroundColor: "#d9e6f2",
+				height:30,
+				top: internalMemSpec.top + 40,
+			});	
+				//pulls ram info from array	
+				var ramSpec = Ti.UI.createLabel({
+					text: this.ram,
+					color: "white",
+					top: ramSpecLabelView.top + ramSpecLabelView.height + 10,
+					left:20
+				});
+		
+		//Creates label for camera size
+		var cameraSpecLabel = Ti.UI.createLabel({
+			text: "Camera:",
+			left:20
+		});
+			//creates view for camera label
+			var cameraSpecView = Ti.UI.createView({
+				backgroundColor: "#d9e6f2",
+				height:30,
+				top: ramSpec.top + 40,
+			});	
 				
-				var returnHome = function(){
-				specWindow.close();
-			};
+				//pulls camera info from array					
+				var cameraSpec = Ti.UI.createLabel({
+					text: this.camera,
+					color: "white",
+					top: cameraSpecView.top + cameraSpecView.height + 10,
+					left:20
+				});
+
+		//Creates label for battery size
+		var batterySpecLabel = Ti.UI.createLabel({
+			text: "Battery:",
+			left:20
+		});
+		
+			//creates view for battery label
+			var batterySpecView = Ti.UI.createView({
+				backgroundColor: "#d9e6f2",
+				height:30,
+				top: cameraSpec.top + 40,
+			});	
+		
+				//pulls battery info from array				
+				var batterySpec = Ti.UI.createLabel({
+					text: this.battery,
+					color: "white",
+					top:batterySpecView.top + batterySpecView.height + 10,
+					left:20
+					});	
 				
+					var returnHome = function(){
+						specWindow.close();
+					};
+		
+		//Adding labels to views and views to windows			
+		screenSizeLabelView.add(screenSizeLabel);
+		internalMemLabelView.add(internalMemLabel);
+		ramSpecLabelView.add(ramSpecLabel);
+		cameraSpecView.add(cameraSpecLabel);
+		batterySpecView.add(batterySpecLabel);	
 		specWindow.add(nameRow);
 		backButton.addEventListener("click", returnHome);
 		backButton.add(backButtonLabel);
 		specTitleBarView.add(specTitleBarText);
-		specWindow.add(specTitleBarView, specTitleBarText, specTitleViewBorder, backButton, specifics);
+		specWindow.add(specTitleBarView, specTitleBarText, specTitleViewBorder, backButton, screenSizeSpec, internalMemSpec, ramSpec, cameraSpec, batterySpec, screenSizeLabelView, internalMemLabelView, ramSpecLabelView, cameraSpecView, batterySpecView);
 		specWindow.open();
 		
 
@@ -119,11 +223,14 @@ var wareableSection = Ti.UI.createTableViewSection({
 for(var i=0, j=phone.length; i<j; i++){
 	var nameRow = Ti.UI.createTableViewRow({
 		title: phone[i].name,
-		internMem: phone[i].InternalMemory,
+		screenSize: phone[i].Screen,
+		internalMem: phone[i].InternalMemory,
+		ram: phone[i].Ram,
+		camera: phone[i].Camera,
+		battery: phone[i].Battery,
 		hasChild: true
 	});
-	
-	
+
 	phoneSection.add(nameRow);
 	nameRow.addEventListener("click", specs);
 };
@@ -132,6 +239,11 @@ for(var i=0, j=phone.length; i<j; i++){
 for(var i=0, j=tablet.length; i<j; i++){
 	var nameRow = Ti.UI.createTableViewRow({
 		title: tablet[i].name,
+		screenSize: tablet[i].Screen,
+		internalMem: tablet[i].InternalMemory,
+		ram: tablet[i].Ram,
+		camera: tablet[i].Camera,
+		battery: tablet[i].Battery,
 		hasChild: true
 	});
 	tabletSection.add(nameRow);
@@ -141,7 +253,12 @@ for(var i=0, j=tablet.length; i<j; i++){
 //To loop through wearables array
 for(var i=0, j=phone.length; i<j; i++){
 	var nameRow = Ti.UI.createTableViewRow({
-		title: phone[i].name,
+		title: wearable[i].name,
+		screenSize: wearable[i].Screen,
+		internalMem: wearable[i].InternalMemory,
+		ram: wearable[i].Ram,
+		camera: wearable[i].Camera,
+		battery: wearable[i].Battery,
 		hasChild: true
 	});
 		
